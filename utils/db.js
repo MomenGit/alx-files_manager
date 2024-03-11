7d4b24dba1c85261760c613378c2505100fdc400
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-11 17:46:58 +0200

    Write utils/db.js

---
+++ utils/db.js
@@ -0,0 +1,53 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const dbPort = process.env.DB_HOST || '27017';
+    const dbHost = process.env.DB_PORT || 'localhost';
+    const dbName = process.env.DB__DATABASE || 'files_manager';
+    const uri = `mongodb://${dbHost}:${dbPort}`;
+    console.log(uri);
+
+    this.client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(this.client);
+
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB connection error:', err);
+      } else {
+        console.log('Connected to MongoDB');
+      }
+    });
+
+    this.db = this.client.db(dbName);
+    console.log(this.db);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      await this.db.collection('users').countDocuments();
+    } catch (error) {
+      console.error(error);
+      await -1;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      await this.db.collection('files').countDocuments();
+    } catch (error) {
+      console.error(error);
+      await -1;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
