5be57d0bb879e5ae2a465ecba3b148d71cf335c3
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-12 23:16:14 +0200

    Add Auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import * as AppController from '../controllers/AppController';
 import * as UsersController from '../controllers/UsersController';
+import * as AuthController from '../controllers/AuthController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
 router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 export default router;
